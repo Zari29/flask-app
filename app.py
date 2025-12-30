a0415513aeb07e3da13081318f993c17494a0504
Zari29 <i221657@nu.edu.pk>
2025-12-30 10:58:50 +0500

    Create database tables on app startup
    Added app context to create database tables on startup.

--- app.py
+++ app.py
@@ -3,39 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Configure SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///firstapp.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-# Define database model
 class Student(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(50), nullable=False)
     lname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False)
 
-    def __repr__(self):
-        return f"{self.sno} - {self.fname}"
-
-# Home route: display records
 @app.route("/")
 def index():
     students = Student.query.all()
     return render_template("index.html", students=students)
 
-# Add new student (CREATE)
 @app.route("/add", methods=["POST"])
 def add():
-    fname = request.form['fname']
-    lname = request.form['lname']
-    email = request.form['email']
-    student = Student(fname=fname, lname=lname, email=email)
+    student = Student(
+        fname=request.form['fname'],
+        lname=request.form['lname'],
+        email=request.form['email']
+    )
     db.session.add(student)
     db.session.commit()
     return redirect("/")
 
-# Delete record (DELETE)
 @app.route("/delete/<int:sno>")
 def delete(sno):
     student = Student.query.filter_by(sno=sno).first()
@@ -43,7 +37,6 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
-# Update record (UPDATE)
 @app.route("/update/<int:sno>", methods=["GET", "POST"])
 def update(sno):
     student = Student.query.filter_by(sno=sno).first()
@@ -56,4 +49,6 @@ def update(sno):
     return render_template("update.html", student=student)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000, debug=False)
